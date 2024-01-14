42d3ac3d5a6bb003e9ea17f7b31af0feea8dac92
Anatolii Konstantinov <anatolii.konstantinov@booking.com>
2024-01-14 21:43:43 +0100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
 
 //const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 const manifestUrl = 'https://aceback04.github.io/tonconnect-manifest.json';
